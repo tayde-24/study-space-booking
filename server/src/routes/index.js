11600cedd4cca9c44cd56b8c79c107231cb1fe7b
Tayde Luevano <tayde_luevano@yahoo.com>
2026-05-09 13:08:41 -0700

    Added some packages and files to start with backend coding.

---
+++ server/src/routes/index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+
+//Change this code
+router.use('/', require('./...'));
---
+++ server/src/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use('/', require('./routes/index'));
